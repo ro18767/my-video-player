36108e353c0837e91f62e80ed731bbda988dab16
ro18767 <108687865+ro18767@users.noreply.github.com>
2024-06-01 17:21:01 +0300

    remove log

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { searchForWorkspaceRoot } from "vite";
 import { defineConfig } from "vitest/config";
 
-console.log('BODY_SIZE_LIMIT', process.env['BODY_SIZE_LIMIT']);
-
-
 export default defineConfig({
   plugins: [sveltekit()],
   // worker: {
