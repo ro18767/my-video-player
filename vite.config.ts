ccbec2365c499a63a07809c21d0ad7c7b35c4893
ro18767 <108687865+ro18767@users.noreply.github.com>
2024-06-01 17:34:28 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { searchForWorkspaceRoot } from "vite";
 import { defineConfig } from "vitest/config";
 
+
+console.log(searchForWorkspaceRoot(process.cwd()));
+
 export default defineConfig({
   plugins: [sveltekit()],
   // worker: {
